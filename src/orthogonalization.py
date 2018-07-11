b0e23dde54dceed0315e743353cacefcd9fd1cd0
Snowpile <erik.r.forsman@gmail.com>
2018-07-11 16:06:05 -0600

    Initial main module

---
+++ src/orthogonalization.py
@@ -0,0 +1,38 @@
+import matplotlib
+import numpy as np
+import pandas as pd
+
+
+def unit_vector(v):
+    """
+    :param v:
+    :return:
+    :exception:
+    """
+
+    return unit_v
+
+
+def proj_subtraction():
+    """
+    :param:
+    :return:
+    :exception:
+    """
+
+    return
+
+
+def main(input_m):
+    """
+    :param input_m:
+    :return:
+    :exception:
+    """
+    
+
+    print("Orthogonalization Matrix:\n")
+    print(o_matrix)
+
+if __name__ == "__main__":
+    main()
