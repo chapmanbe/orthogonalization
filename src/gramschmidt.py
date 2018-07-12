b7f16dd0d8707302ce02de8b772a4e043e07dc42
Anwar <alsaneaan@gmail.com>
2018-07-12 09:16:10 -0600

    Anwar final

---
+++ src/gramschmidt.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def unit_vector(vec):
+    """
+    This function will create a unit vector version of the inputted vector
+    This returns a vector
+    vec: is a numpy array vector
+
+    """
+    pass
+
+
+def gauss_elim(matrix):
+    """
+    This function will take a matrix that is non-orthonormal and will
+    return a matrix that is orthonormalself.
+
+    Check if matrix is orthonormal and return a string that says:
+    "matrix is already orthonormal"
+
+    raise a ValueError if something other than a matrix is inputted into
+    the function
+
+    """
