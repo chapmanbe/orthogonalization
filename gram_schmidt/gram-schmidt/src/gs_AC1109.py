e44f44ad30b36c7027fa46aa063151e952289cca
zhenyulu2 <zhenyulu2@gmail.com>
2018-07-12 12:28:47 -0600

    change nothing but add description before the code

--- gram_schmidt/gram-schmidt/src/gs_AC1109.py
+++ gram_schmidt/gram-schmidt/src/gs_AC1109.py
@@ -21,16 +21,39 @@ check_dots
 '''
 
 def unit_vector(v):
+    """
+    Takes a vector v and returns a unit vector form
+    v: numpy array
+    Returns :
+        numpy array
+    Exceptions:
+        ValueError
+    """
+    
     return v/la.norm(v)
 
 
 def subtract_projection(a, q):
     """
     subtract projection of a onto q from a
+    Takes two vectors and substracts the projection of a onto q from a
+    a : numpy array
+    q: numpy array
+    Retruns:
+        numpy array
+    Exceptions:
+        ValueError: if a and q are not the same dimension
     """
     return a -float(q.transpose() * a) * q
 
 def gram-schmidt(A):
+    """
+    Takes a matrix and returns an orthonormal "version" suing Gram-Schmidt 
+    a: numpy array
+    Returns:
+        numpy array
+    Exceptions:
+    """
     Q = A.copy().astype(np.float64)
     n = A.shape[1]
     for i in range(A.shape[1]):
