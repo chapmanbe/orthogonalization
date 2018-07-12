18081ce7c5c0d9f820d35407a5467e6ef50c025e
Brian Chapman <brian.chapman@utah.edu>
2018-07-12 10:30:29 -0600

    added my first gramschmidt specifications

---
+++ src/gramschmidt.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+def unit_vector(v):
+    """
+    Takes a vector v and returns a unit vector form
+
+    v: numpy array
+
+    Returns:
+        numpy array
+
+    Exceptions:
+        ValueError
+
+    """
+    pass
+
+def subtract_projection(a, q):
+    """
+    Takes two vectors and subtracts the projection of a onto q from a
+
+    a: numpy array
+    q: numpy array
+
+    returns: numpy array
+
+    Exceptions:
+       ValueError: if a and q are not the same dimension
+    """
+
+def gram_schmidt(a):
+    """
+    Takes a matrix and returns an orthonormal 'version' using
+    Gram-Schmidt orthogonilization
+
+    a: an mxn numpy array
+
+    Returns: a numpy array
+
+    Exceptions:
+        ValueError
+    """
+
+    pass
