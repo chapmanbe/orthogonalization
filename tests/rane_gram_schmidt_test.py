55f765b6cf5745101350e6fbad0480430b163187
Rane_Thomas <rane.thomas@gmail.com>
2018-07-12 10:52:32 -0600

    Incomplete tests for orthogonality functions

---
+++ tests/rane_gram_schmidt_test.py
@@ -0,0 +1,17 @@
+import pytest
+import numpy as np
+import gram_schmidt as gs
+
+def test_unit_vector1():
+    v = np.zeros([2,1])
+    with pytest.raises(ValueError):
+        gs.unit_vector(v)
+
+def test_unit_vector2():
+    v = np.ones([2,1])
+    new_v = gs.unit_vector(v)
+    assert np.abs(np.norm(new_v) - 1.0) < 0.00001
+
+
+def test_subtract_projection():
+    v =
